7d5c885dbfd42cebb87e7098015cbffdb66a3786
mohan4 <mohan@afixi.com>
2021-12-26 18:38:40 +0530

    graph

--- module/dashboard.php
+++ module/dashboard.php
@@ -11,9 +11,16 @@ class dashboard extends common {
     }
     function show() {
         $id = $_SESSION['id_user'];
-        $sql = "SELECT MONTHNAME(date) AS month, YEAR(date) AS year, MONTH(date) as m, SUM(total) AS total FROM {$this->prefix}sale WHERE id_head='$id' GROUP BY 1,2,3 ORDER BY 2,3";
-        $purc = $this->m->getall($this->m->query($sql));
-        $this->sm->assign("purc", $purc);
+        $sdate = $_SESSION['start_date'];
+        $edate = $_SESSION['end_date'];
+        $sql = "SELECT MONTHNAME(date) AS month, YEAR(date) AS year, MONTH(date) as m, SUM(total) AS total FROM {$this->prefix}sale WHERE id_head='$id' AND date BETWEEN '$sdate' AND '$edate' GROUP BY 1,2 ORDER BY 2,3";
+        $this->sm->assign("purc", $this->m->getall($this->m->query($sql)));
+
+        $sql = "SELECT month, year, m, SUM(purcqty) AS purcqty, SUM(saleqty) AS saleqty FROM (SELECT MONTHNAME(date) AS month, YEAR(date) AS year, MONTH(date) as m, SUM(qty+free) AS purcqty, 0 AS saleqty FROM {$this->prefix}saledetail WHERE id_head='$id' AND date BETWEEN '$sdate' AND '$edate' GROUP BY 1,2 
+        UNION ALL 
+        SELECT MONTHNAME(date) AS month, YEAR(date) AS year, MONTH(date) as m, 0 AS purcqty, SUM(qty) AS saleqty FROM {$this->prefix}partner_stock WHERE id_head='$id' AND date BETWEEN '$sdate' AND '$edate' GROUP BY 1,2) a GROUP BY 1,2 ORDER BY 2,3";
+        $p = $this->m->getall($this->m->query($sql));
+        $this->sm->assign("qty", $p);
     }
 }
 ?>
